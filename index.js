0eae2553f365966670ab916be7f3d00fc1302d5b
Reza12121 <75201148+Reza12121@users.noreply.github.com>
2022-11-02 10:52:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your fav video</title>
-    <link rel="stylesheet" href="index.css">
+"use strict";
 
-    <script src="index.js" type="module"></script>
-</head>
-<body>
-    <center>
-    <h1>Quality Switching Evaluation Survey</h1>
-    <br>
-    <br>
-    <h2>Step 1</h2>
-    <p>
-       Answer the survey: <a href="https://docs.google.com/forms/d/e/1FAIpQLSeMkWbULbp74L-qpzv0wdrSoRfBng848hgj0YuuOA3AZSY5Sg/viewform?usp=sf_link" target="_blank">Survey Link</a>
-    </p>
-    <br>
-    <h2>Step 2</h2>
-    <p>
-        Watch the video (Once only, and preferably in full screen)
-     </p>
-     <br>
-    <div class="wrapper">
-        <video src="#" id="video" controls></video>
-    </div>
+const page = "home";
+const openRequest = indexedDB.open("videos", 1);
 
-    <!DOCTYPE html>
-    <br>
-      <h2>Step 3</h2>
-      <p>
-         Answer the survey: <a href="https://docs.google.com/forms/d/e/1FAIpQLSfczZqsvkeCjgP-yR1FaGMzp5UsSHek0p2BIlbKSCGpspLffQ/viewform?usp=sf_link" target="_blank">Survey Link</a>
-      </p>
-      <br>
-      <br>
-      <h4>Thanks for participating!</h4>
-    </center>
+openRequest.addEventListener("upgradeneeded", function (event) {
+	let db = openRequest.result;
+	if (!db.objectStoreNames.contains("video")) {
+		db.createObjectStore("video", { keyPath: "page" });
+	}
+});
 
-</body>
-</html>
+let dataBase;
+openRequest.addEventListener("success", () => {
+	dataBase = openRequest.result;
+
+	let transaction = dataBase.transaction("video", "readonly");
+	let objStore = transaction.objectStore("video");
+
+	const data = objStore.get(page);
+	data.addEventListener("success", () => {
+		if (data.result) {
+			showVid(data.result.blob);
+		} else {
+			addVidToDatabase();
+		}
+	});
+
+	dataBase.addEventListener("error", function (event) {
+		let request = event.target;
+
+		console.log("Error", request.error);
+	});
+});
+
+async function addVidToDatabase() {
+	const res = await fetch("./vid.mp4");
+	const blob = await res.blob();
+
+	const obj = {
+		blob,
+		page,
+	};
+
+	let transaction = dataBase.transaction("video", "readwrite");
+	let objStore = transaction.objectStore("video");
+	objStore.add(obj);
+
+	showVid(blob);
+}
+
+function showVid(vid) {
+	document.getElementById("video").src = URL.createObjectURL(vid);
+}
