dd0da4f92670bfae174fb4886aa268b91cff0aff
Reza12121 <75201148+Reza12121@users.noreply.github.com>
2022-11-04 10:23:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const express = require('express')
-const app = express ()
+const express = require('express');
 const fs = require('fs');
-const url = require("url");
-const path = require("path");
+const url = require('url');
+const path = require('path');
+
+const app = express();
 
 const MIME_TYPE = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png",
-    "svg": "image/svg+xml",
-    "json": "application/json",
-    "js": "application/javascript",
-    "css": "text/css",    
-    "mpd":"application/dash+xml",
-    "mp4": "video/mp4",    
-    "m4s": "video/mp4"
-  };
+    '.css': 'text/css',
+    '.html': 'text/html',
+    '.jpeg': 'image/jpeg',
+    '.jpg': 'image/jpeg',
+    '.js': 'application/javascript',
+    '.json': 'application/json',
+    '.m4s': 'video/mp4',
+    '.mp4': 'video/mp4',
+    '.mpd': 'application/dash+xml',
+    '.png': 'image/png',
+    '.svg': 'image/svg+xml',
+};
 
-function getMimeType(filename)
-{
-    var mimeType = MIME_TYPE[filename.split('.').pop()];
-    console.log("Mime Type = " + mimeType);
-    if (!mimeType) {
+function getMimeType(filename) {
+    const ext = path.extname(filename);
+    let mimeType = MIME_TYPE[ext];
+    console.log('Mime Type: ' + mimeType);
+    if (mimeType === undefined) {
         mimeType = 'text/plain';
     }
     return mimeType;
@@ -31,45 +32,34 @@ function getMimeType(filename)
 //const DEFAULT_CONTENT_DIRECTORY = __dirname + '/content';
 const DEFAULT_INDEX_HTML = __dirname + '/index.html';
 
-
 app.get('/', (req, res) => {
-       
-    var filename = DEFAULT_INDEX_HTML;
-    console.log("Req url = " + req.url);
-    fs.readFile(filename, "binary", function(err, file) {
-        if(err) {        
-            res.send('Could not find default index.html');
-            res.end();
+    console.log('Requested URL: ' + req.url)
+    const filename = DEFAULT_INDEX_HTML;
+    fs.readFile(filename, 'binary', function (err, file) {
+        if (err) {
+            res.send('Could not find: index.html');
+            res.end('Could not find: index.html');
             return;
         }
-        
         res.set('Content-Type', 'text/html');
-        //res.send(file);
-        //res.end();
         res.end(Buffer.from(file, 'binary'));
-    });    
-})
+    });
+});
 
-app.get('/content*', (req, res) => {
-    //console.log('Requested URL = ' + req.url)
-    //res.send('Get the article');
-    var filename = __dirname + req.url;
-    console.log("Filename = " + filename);
-    fs.readFile(filename, "binary", function(err, file) {
-        if(err) {  
-            console.log('Could not find resource = ' + filename);      
-            res.send('could not fint item = ' + req.url);
+app.get('/*', (req, res) => {
+    console.log('Requested URL: ' + req.url)
+    const filename = __dirname + req.url;
+    fs.readFile(filename, 'binary', function (err, file) {
+        if (err) {
+            console.log('Could not find resource: ' + filename);
+            res.send('Could not find resource: ' + req.url);
             return;
-        }        
+        }
         res.set('Content-Type', getMimeType(filename));
-        //res.send(file);
-        //res.end();
         res.end(Buffer.from(file, 'binary'));
-    });    
+    });
+});
 
-})
-
-
-app.listen(3000)
-console.log('running!')
+app.listen(3000);
+console.log('running!');
 //console.log('deafult dir' + DEFAULT_CONTENT_DIRECTORY)
