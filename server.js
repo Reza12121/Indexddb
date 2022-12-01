62cf4d1398a6f500a58b97832a35934b66208e48
Reza12121 <75201148+Reza12121@users.noreply.github.com>
2022-12-01 19:33:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const randomUUID = self.crypto.randomUUID();
 
 const { MongoClient } = require('mongodb');
 const uri = `mongodb://127.0.0.1:27017`;
